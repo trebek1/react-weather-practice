2660fc69dba7bf288a833475ba7488ae0f987fb6
trebek1 <trebek08@gmail.com>
2016-09-11 00:28:27 -0700

    bundle

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var Nav = React.createClass({
 	render: function(){
 		return(
-			<h2> Nav Component </h2>
+			<div>
+				<h2> Nav Component </h2>
+			</div>
+			
 		)
 	}
 });
